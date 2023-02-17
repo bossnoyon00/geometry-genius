be16eef84fdee4f791222e8037e493389dbba32e
bossnoyon00 <bossnoyon584@gmail.com>
2023-02-18 03:35:02 +0600

    add another calculation part

--- js/index.js
+++ js/index.js
@@ -11,9 +11,18 @@ document.getElementById('first-btn').addEventListener('click', function () {
     const secondInput = document.getElementById('second-input');
     const secondInputString = secondInput.value;
     const secondInputTotal = parseFloat(secondInputString);
-
+    if (isNaN(firstInputTotal && secondInputTotal)) {
+        return alert('please add a valid number')
+    }
+    else if (firstInputTotal < 0) {
+        return alert('Please add should be a positive number');
+    }
+    else if (secondInputTotal < 0) {
+        return alert('Please add should be a positive number');
+    }
     const addCalcu = 0.5 * firstInputTotal * secondInputTotal;
-    createElement(firstName, addCalcu, serial);
+    const toFixeds = parseFloat(addCalcu.toFixed(2))
+    createElement(firstName, toFixeds, serial);
 })
 document.getElementById('second-btn').addEventListener('click', function () {
     serial += 1;
@@ -27,13 +36,36 @@ document.getElementById('second-btn').addEventListener('click', function () {
     const secondInput = document.getElementById('rec-input-two');
     const secondInputString = secondInput.value;
     const secondInputTotal = parseFloat(secondInputString);
+    if (isNaN(firstInputTotal && secondInputTotal)) {
+        return alert('please add a valid number')
+    }
+    else if (firstInputTotal < 0) {
+        return alert('Please add should be a positive number');
+    }
+    else if (secondInputTotal < 0) {
+        return alert('Please add should be a positive number');
+    }
+    const addCalcu = firstInputTotal * secondInputTotal;
+    const toFixeds = parseFloat(addCalcu.toFixed(2))
+    createElement(firstName, toFixeds, serial);
+})
+document.getElementById('third-btn').addEventListener('click', function () {
+    serial += 1;
+    const firstName = document.getElementById('third-name').innerText;
+
+    const firstInput = document.getElementById('para-input-one');
+    const firstInputString = firstInput.value;
+    const firstInputTotal = parseFloat(firstInputString);
+
+
+    const secondInput = document.getElementById('para-input-two');
+    const secondInputString = secondInput.value;
+    const secondInputTotal = parseFloat(secondInputString);
 
     const addCalcu = firstInputTotal * secondInputTotal;
-    createElement(firstName, addCalcu, serial);
+    const toFixeds = parseFloat(addCalcu.toFixed(2))
+    createElement(firstName, toFixeds, serial);
 })
-
-
-
 
 
 
