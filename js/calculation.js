5cb288b752de4c82744730df1371b976efe0789f
bossnoyon00 <bossnoyon584@gmail.com>
2023-02-18 06:18:41 +0600

    Addeed function and use carefully

--- js/calculation.js
+++ js/calculation.js
@@ -1,8 +1,11 @@
+//Serial no:
 let serial = 0;
+
+
+//First card calculation
 document.getElementById('first-btn').addEventListener('click', function () {
     serial += 1;
-    const firstName = document.getElementById('first-name').innerText;
-
+    const firstName =getElementValueById('first-name');
     const firstInput = document.getElementById('first-input');
     const firstInputString = firstInput.value;
     const firstInputTotal = parseFloat(firstInputString);
@@ -20,13 +23,19 @@ document.getElementById('first-btn').addEventListener('click', function () {
     else if (secondInputTotal < 0) {
         return alert('Please add should be a positive number');
     }
+
+    //Calculation
     const addCalcu = 0.5 * firstInputTotal * secondInputTotal;
     const toFixeds = parseFloat(addCalcu.toFixed(2))
     createElement(firstName, toFixeds, serial);
+
+
 })
+
+//second card calculation
 document.getElementById('second-btn').addEventListener('click', function () {
     serial += 1;
-    const firstName = document.getElementById('second-name').innerText;
+    const firstName =getElementValueById('second-name');
 
     const firstInput = document.getElementById('rec-input-one');
     const firstInputString = firstInput.value;
@@ -45,87 +54,102 @@ document.getElementById('second-btn').addEventListener('click', function () {
     else if (secondInputTotal < 0) {
         return alert('Please add should be a positive number');
     }
+
+    //Calculation
     const addCalcu = firstInputTotal * secondInputTotal;
     const toFixeds = parseFloat(addCalcu.toFixed(2))
     createElement(firstName, toFixeds, serial);
 })
+
+//Third card calculation
 document.getElementById('third-btn').addEventListener('click', function () {
     serial += 1;
-    const firstName = document.getElementById('third-name').innerText;
 
-    const firstInput = document.getElementById('para-input-one');
-    const firstInputString = firstInput.value;
+    const firstName = getElementValueById('third-name');
+    const firstInputString = getElementValueById('para-input-one');
     const firstInputTotal = parseFloat(firstInputString);
 
 
-    const secondInput = document.getElementById('para-input-two');
-    const secondInputString = secondInput.value;
-    const secondInputTotal = parseFloat(secondInputString);
+    const secondInput = getElementValueById('para-input-two');
+    const secondInputTotal = parseFloat(secondInput);
 
+    //Calculation
     const addCalcu = firstInputTotal * secondInputTotal;
     const toFixeds = parseFloat(addCalcu.toFixed(2))
     createElement(firstName, toFixeds, serial);
 })
+
+// //Fourth card calculation
 document.getElementById('fourth-btn').addEventListener('click', function () {
     serial += 1;
-    const firstName = document.getElementById('fourth-name').innerText;
+    const firstName = getElementValueById('fourth-name');
 
-    const firstInput = document.getElementById('rhombus-input-one');
-    const firstInputString = firstInput.value;
-    const firstInputTotal = parseFloat(firstInputString);
+    const firstInput = getElementValueById('rhombus-input-one');
+    const firstInputTotal = parseFloat(firstInput);
 
 
-    const secondInput = document.getElementById('rhombus-input-two');
-    const secondInputString = secondInput.value;
-    const secondInputTotal = parseFloat(secondInputString);
+    const secondInput = getElementValueById('rhombus-input-two');
+    const secondInputTotal = parseFloat(secondInput);
 
+    //Calculation
     const addCalcu = 0.5 * firstInputTotal * secondInputTotal;
     const toFixeds = parseFloat(addCalcu.toFixed(2))
     createElement(firstName, toFixeds, serial);
 })
+
+
+//Fifth card calculation
 document.getElementById('fifth-btn').addEventListener('click', function () {
     serial += 1;
-    const firstName = document.getElementById('fifth-name').innerText;
+    const firstName = getElementValueById('fifth-name');
 
-    const firstInput = document.getElementById('pen-input-one');
-    const firstInputString = firstInput.value;
-    const firstInputTotal = parseFloat(firstInputString);
+    const firstInput = getElementValueById('pen-input-one');
+    const firstInputTotal = parseFloat(firstInput);
 
 
-    const secondInput = document.getElementById('pen-input-two');
-    const secondInputString = secondInput.value;
-    const secondInputTotal = parseFloat(secondInputString);
+    const secondInput = getElementValueById('pen-input-two');
+    const secondInputTotal = parseFloat(secondInput);
 
+
+    //Calculation
     const addCalcu = 0.5 * firstInputTotal * secondInputTotal;
     const toFixeds = parseFloat(addCalcu.toFixed(2))
     createElement(firstName, toFixeds, serial);
 })
+
+//Six card calculation
 document.getElementById('six-btn').addEventListener('click', function () {
     serial += 1;
-    const firstName = document.getElementById('six-name').innerText;
+    const firstName = getElementValueById('six-name');
 
-    const firstInput = document.getElementById('ellipse-input-one');
-    const firstInputString = firstInput.value;
-    const firstInputTotal = parseFloat(firstInputString);
+    const firstInput = getElementValueById('ellipse-input-one');
+    const firstInputTotal = parseFloat(firstInput);
 
 
-    const secondInput = document.getElementById('ellipse-input-two');
-    const secondInputString = secondInput.value;
-    const secondInputTotal = parseFloat(secondInputString);
+    const secondInput = getElementValueById('ellipse-input-two');
+    const secondInputTotal = parseFloat(secondInput);
 
+
+    //Calculation
     const addCalcu = Math.PI * firstInputTotal * secondInputTotal;
     const toFixeds = parseFloat(addCalcu.toFixed(2))
     createElement(firstName, toFixeds, serial);
 })
 
 
+//New page Link
 document.getElementById('new-page').addEventListener('click', function () {
     location.href = 'index2.html'
 })
 
 
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId).innerText;
+    return element;
+}
 
 
+//Create a function to create all <td> element
 function createElement(firstName, addCalcu, serial) {
     const container = document.getElementById('table-container');
     const tr = document.createElement("tr");
